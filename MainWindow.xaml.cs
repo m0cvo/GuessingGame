eebc70d38084aa4caa71dcf7b0d4914ac21bea98
Nigel <m0cvo@yahoo.co.uk>
2022-03-08 09:59:07 +0000

    Code behind

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,5 +29,12 @@ namespace GuessingGame
         {
             Application.Current.Shutdown();
         }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Hide();
+        }
     }
 }
